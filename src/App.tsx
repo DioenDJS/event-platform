b4fbc96d0e2634fea656462e190a210371602ce4
Diovane Pacheti <negociospacheti@gmail.com>
2022-06-21 19:31:34 -0300

    :art: add components in file App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Header } from "./components/Header"
+import { Lesson } from "./components/Lesson"
+import { SideBar } from "./components/SideBar"
+import { Video } from "./components/Video"
+
 function App() {
 
   return (
    <div>
+    <Header />
+    <SideBar />
+    <Video />
+    <Lesson />
+
    </div>
   )
 }
