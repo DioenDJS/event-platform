10593550234516e6a444644d5d3759df3ce085e6
Diovane Pacheti <negociospacheti@gmail.com>
2022-06-21 19:37:43 -0300

    :art: Create folder pages and add in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { Header } from "./components/Header"
-import { Lesson } from "./components/Lesson"
-import { SideBar } from "./components/SideBar"
-import { Video } from "./components/Video"
+import { Event } from "./pages/Event"
 
 function App() {
 
   return (
    <div>
-    <Header />
-    <SideBar />
-    <Video />
-    <Lesson />
-
+    <Event />
    </div>
   )
 }
