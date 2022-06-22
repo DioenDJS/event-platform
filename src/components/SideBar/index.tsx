57fb5671caf38dbf0979f35ad0d8a3e56903b92c
Diovane Pacheti <negociospacheti@gmail.com>
2022-06-22 02:53:31 -0300

    :art: add title sideBar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,7 +1,9 @@
 export function SideBar(){
     return(
         <aside className=" w-[345px] bg-gray-700 p-6 border-l border-gray-600">
-
+            <span className="font-bold text-2xl pb-6 border-6 border-gray-500 block">
+                Cronograma de aulas
+            </span>
         </aside>
     )
 }
