c8ce93951162b977a7bbb367694a8e47b62067b8
Diovane Pacheti <negociospacheti@gmail.com>
2022-06-21 21:44:16 -0300

    :art: add tag main  and component Video / SideBar

--- src/pages/Event/index.tsx
+++ src/pages/Event/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from "../../components/Header";
+import { SideBar } from "../../components/SideBar";
+import { Video } from "../../components/Video";
 
 export function Event(){
     return(
         <>
             <Header />
-            
+            <main>
+                <Video />
+                <SideBar />
+            </main>
         </>        
     )
 }
