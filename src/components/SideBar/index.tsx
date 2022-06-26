cf2c0bf5471976d551fa0f59c7b44f3e940e8d2c
Diovane Pacheti <negociospacheti@gmail.com>
2022-06-26 17:02:25 -0300

    :art: add use hook useGetLessonsQuery

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,8 +1,10 @@
-import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "../Lesson";
+import { useGetLessonsQuery } from "../Lesson/queries.generated";
 
 export function SideBar(){
-    const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+
+    const { data } = useGetLessonsQuery()
+    
     return(
         <aside className=" w-[345px] bg-gray-700 p-6 border-l border-gray-600">
             <span className="font-bold text-2xl pb-6 border-6 border-gray-500 block">
